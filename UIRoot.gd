0caf670918aaf92771db7806f14a0e6d63de1a8a
dan branton <dtotheb@gmail.com>
2024-02-25 21:00:07 -0400

    basic crafting works now

--- UIRoot.gd
+++ UIRoot.gd
@@ -5,9 +5,11 @@ extends CanvasLayer
 @onready var player = %Player
 # Called when the node enters the scene tree for the first time.
 const COIN_STACK = preload("res://recipes/coinStack.tres")
+const CROWN_1 = preload("res://recipes/crown1.tres")
+const SPLIT_COINS = preload("res://recipes/splitCoins.tres")
 
 func _unhandled_input(event):
 	if event.is_action_released("inventory"):
 		inventory_dialog.open(player.inventory)
 	if event.is_action_released("crafting"):
-		crafting_dialog.open([COIN_STACK],player.inventory)
+		crafting_dialog.open([COIN_STACK, SPLIT_COINS,CROWN_1],player.inventory)
--- inventoryDialog/item_slot.gd
+++ inventoryDialog/item_slot.gd
@@ -1,10 +1,12 @@
 extends PanelContainer
 @onready var texture_rect:TextureRect = %TextureRect
 
+var _item:Item
 
 func display(item:Item):
+	_item = item
 	texture_rect.texture = item.icon	
 	
 
 func _on_texture_rect_mouse_entered():
-	print("on texture mouse enter ") # Replace with function body.
+	print(_item.name)
